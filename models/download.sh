464811d9c97c491f0aba673eed3e2481f974a822
Vilém Zouhar <vilem.zouhar@gmail.com>
2020-12-17 21:24:47 +0100

    add inference and download scripts

--- models/download.sh
+++ models/download.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-wget -nc -P ende/ http://data.statmt.org/romang/bergamot/models/deen/ende.student.base/model.npz
-wget -nc -P ende/ http://data.statmt.org/romang/bergamot/models/deen/ende.student.tiny11/lex.s2t.gz
-wget -nc -P ende/ http://data.statmt.org/romang/bergamot/models/deen/vocab.deen.spm
+# run me from the model directory
 
+# ende
+wget -nc -P teacher/ende/ http://data.statmt.org/romang/bergamot/models/deen/ende.student.base/model.npz
+wget -nc -P teacher/ende/ http://data.statmt.org/romang/bergamot/models/deen/ende.student.tiny11/lex.s2t.gz
+wget -nc -P teacher/ende/ http://data.statmt.org/romang/bergamot/models/deen/vocab.deen.spm
+mv teacher/ende/vocab.deen.spm teacher/ende/vocab.spm
 
-wget -nc -P csen/ http://data.statmt.org/bergamot/models/8bit-students/csen/base/model.npz.best-bleu-detok.npz
-wget -nc -P csen/ http://data.statmt.org/bergamot/models/8bit-students/csen/base/lex.s2t.gz
-wget -nc -P csen/ http://data.statmt.org/bergamot/models/8bit-students/csen/base/vocab.spm
+# csen
+wget -nc -P teacher/csen/ http://data.statmt.org/bergamot/models/8bit-students/csen/base/model.npz.best-bleu-detok.npz
+wget -nc -P teacher/csen/ http://data.statmt.org/bergamot/models/8bit-students/csen/base/lex.s2t.gz
+wget -nc -P teacher/csen/ http://data.statmt.org/bergamot/models/8bit-students/csen/base/vocab.spm
+mv teacher/csen/model.npz.best-bleu-detok.npz teacher/csen/model.npz
 
-wget -nc -P encs/ http://data.statmt.org/bergamot/models/8bit-students/encs/base/model.npz.best-bleu-detok.npz
-wget -nc -P encs/ http://data.statmt.org/bergamot/models/8bit-students/encs/base/lex.s2t.gz
-wget -nc -P encs/ http://data.statmt.org/bergamot/models/8bit-students/encs/base/vocab.spm
+# encs
+wget -nc -P teacher/encs/ http://data.statmt.org/bergamot/models/8bit-students/encs/base/model.npz.best-bleu-detok.npz
+wget -nc -P teacher/encs/ http://data.statmt.org/bergamot/models/8bit-students/encs/base/lex.s2t.gz
+wget -nc -P teacher/encs/ http://data.statmt.org/bergamot/models/8bit-students/encs/base/vocab.spm
+mv teacher/encs/model.npz.best-bleu-detok.npz teacher/encs/model.npz
