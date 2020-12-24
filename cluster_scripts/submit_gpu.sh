ae0d4d0a5faed5af9084e51458780b5700cb1222
Vilém Zouhar <vilem.zouhar@gmail.com>
2020-12-24 15:17:17 +0100

    automated experiment job submitting

--- cluster_scripts/submit_gpu.sh
+++ cluster_scripts/submit_gpu.sh
@@ -2,14 +2,38 @@
 
 mkdir -p cluster_logs
 
-filename=$(basename -- "$1")
-filename="${filename%.*}"
+EXPERIMENTS=$1
+for EXP in $EXPERIMENTS; do
+    echo "Experiment $EXP, language cs-en, source: {train,eval}.cs-en.{cs,en}"
+    read _
+    qsubmit \
+        -q 'gpu*' \
+        -l gpu=4,gpu_ram=3G \
+        -pe smp 4 \
+        -cwd \
+        -o ./cluster_logs/$EXP.csen.olog \
+        -e ./cluster_logs/$EXP.csen.elog \
+        "cluster_scripts/train_experiment.sh $EXP cs-en cs en"
 
-qsub \
-    -q 'gpu*' \
-    -l gpu=6,gpu_ram=3G \
-    -pe smp 6 \
-    -cwd \
-    -o ./cluster_logs/$filename.olog \
-    -e ./cluster_logs/$filename.elog \
-    cluster_scripts/$filename.sh
+    echo "Experiment $EXP, language en-cs, source: {train,eval}.cs-en.{en,cs}"
+    read _
+    qsubmit \
+        -q 'gpu*' \
+        -l gpu=4,gpu_ram=3G \
+        -pe smp 4 \
+        -cwd \
+        -o ./cluster_logs/$EXP.encs.olog \
+        -e ./cluster_logs/$EXP.encs.elog \
+        "cluster_scripts/train_experiment.sh $EXP cs-en en cs"
+
+    echo "Experiment $EXP, language en-de, source: {train,eval}.de-en.{en,de}"
+    read _
+    qsubmit \
+        -q 'gpu*' \
+        -l gpu=4,gpu_ram=3G \
+        -pe smp 4 \
+        -cwd \
+        -o ./cluster_logs/$EXP.ende.olog \
+        -e ./cluster_logs/$EXP.ende.elog \
+        "cluster_scripts/train_experiment.sh $EXP de-en en de"
+done
---
+++ cluster_scripts/train_experiment.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# run me from the top directory
+
+EXP=$1
+LANGSOURCE=$2
+LANG1=$3
+LANG2=$4
+
+mkdir -p models/experiment/$EXP/$LANG1$LANG2
+
+marian \
+    -m models/experiment/$EXP/$LANG1$LANG2/model.bin \
+    -c models/experiment/config.yml \
+    -t data/original/train.$LANGSOURCE.$LANG1 data/original/train.$LANGSOURCE.$LANG2 \
+    -v models/experiment/$EXP/$LANG1$LANG2/vocab.spm models/experiment/$EXP/$LANG1$LANG2/vocab.spm \
+    --valid-sets data/original/eval.$LANGSOURCE.$LANG1 data/original/eval.$LANGSOURCE.$LANG2 \
+    --devices 0 1 2 3 \
+    --valid-metrics bleu \
+    --overwrite \
+    --keep-best \
+    --early-stopping 10 \
