1b5daa32e1952f8f39aeb32e798a94d4eb51f5ba
Vilém Zouhar <vilem.zouhar@gmail.com>
2020-12-26 22:20:14 +0100

    update g1, g2

--- src/create_data.py
+++ src/create_data.py
@@ -49,10 +49,10 @@ META_RECIPES = {
         (1, top_k(scorer=lambda x: x.spm_diff(), k=1)),
     ]),
     'g1': aggregator(recipe=[
-        (1, atleast(scorer=lambda x: x.bleu(), threshold=70)),
+        (1, atleast(scorer=lambda x: x.bleu(), threshold=50)),
     ]),
     'g2': aggregator(recipe=[
-        (1, atleast(scorer=lambda x: x.ter(), threshold=0.6)),
+        (1, atleast(scorer=lambda x: x.ter(), threshold=0.8)),
     ]),
 }
 
