9d37cea22675f202c77b8baaee648c75f612550b
Vilém Zouhar <vilem.zouhar@gmail.com>
2020-12-28 11:46:59 +0100

    student infer submitter

--- cluster_scripts/infer_student_test.sh
+++ cluster_scripts/infer_student_test.sh
@@ -4,13 +4,11 @@
 
 EXP=$1
 
-mkdir -p data/teacher
-
 marian-decoder \
     -m "models/experiment/$EXP/encs/model.bin.best-bleu.bin" \
-    -v "models/experiment/$EXP/encs/vocab.spm" "models/teacher/encs/vocab.spm" \
+    -v "models/experiment/$EXP/encs/vocab.spm" "models/experiment/$EXP/encs/vocab.spm" \
     -i "data/original/test.cs-en.en" \
-    -o "data/experiment/$EXP/test.cs-en.en" \
+    -o "data/experiment/$EXP/test.cs-en.cs" \
     --devices 0 1 2 3 \
     --quiet-translation \
 
@@ -18,7 +16,7 @@ marian-decoder \
     -m "models/experiment/$EXP/csen/model.bin.best-bleu.bin" \
     -v "models/experiment/$EXP/csen/vocab.spm" "models/experiment/$EXP/csen/vocab.spm" \
     -i "data/original/test.cs-en.cs" \
-    -o "data/experiment/$EXP/test.cs-en.cs" \
+    -o "data/experiment/$EXP/test.cs-en.en" \
     --devices 0 1 2 3 \
     --quiet-translation \
     
@@ -26,6 +24,6 @@ marian-decoder \
     -m "models/experiment/$EXP/ende/model.bin.best-bleu.bin" \
     -v "models/experiment/$EXP/ende/vocab.spm" "models/experiment/$EXP/ende/vocab.spm" \
     -i "data/original/test.de-en.en" \
-    -o "data/experiment/$EXP/test.de-en.en" \
+    -o "data/experiment/$EXP/test.de-en.de" \
     --devices 0 1 2 3 \
     --quiet-translation \
---
+++ cluster_scripts/submit_infer_student.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+EXPERIMENTS=$1
+for EXP in $EXPERIMENTS; do
+    echo -en "\nInfer $EXP"
+    read _
+    echo -e "#!/bin/bash\n./cluster_scripts/infer_student_test.sh $EXP" > tmp/i.$EXP.sh
+    qsub \
+        -l gpu=3,gpu_ram=1G \
+        -pe smp 3 \
+        -cwd \
+        -j y \
+        -o ./cluster_logs/i.$EXP.log \
+        tmp/i.$EXP.sh
+done
