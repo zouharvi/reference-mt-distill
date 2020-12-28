cda1942dc36e05cae35b0b9f0a7a4acc6cf92266
Vilém Zouhar <vilem.zouhar@gmail.com>
2020-12-28 09:53:33 +0100

    add infer student script

---
+++ cluster_scripts/infer_student_test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# run me from the top directory
+
+EXP=$1
+
+mkdir -p data/teacher
+
+marian-decoder \
+    -m "models/experiment/$EXP/encs/model.bin" \
+    -v "models/experiment/$EXP/encs/vocab.spm" "models/teacher/encs/vocab.spm" \
+    -i "data/original/test.cs-en.en" \
+    -o "data/experiment/$EXP/test.cs-en.en" \
+    --devices 0 1 2 3 \
+    --quiet-translation \
+
+marian-decoder \
+    -m "models/experiment/$EXP/csen/model.bin" \
+    -v "models/experiment/$EXP/csen/vocab.spm" "models/experiment/$EXP/csen/vocab.spm" \
+    -i "data/original/test.cs-en.cs" \
+    -o "data/experiment/$EXP/test.cs-en.cs" \
+    --devices 0 1 2 3 \
+    --quiet-translation \
+    
+marian-decoder \
+    -m "models/experiment/$EXP/ende/model.npz" \
+    -v "models/experiment/$EXP/ende/vocab.spm" "models/experiment/$EXP/ende/vocab.spm" \
+    -i "data/original/test.de-en.en" \
+    -o "data/experiment/$EXP/test.de-en.en" \
+    --devices 0 1 2 3 \
+    --quiet-translation \
